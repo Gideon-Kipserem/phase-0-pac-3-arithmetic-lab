bdc795287e7c9dfd3572e20e66f6785d6e65ab63
Gideon <gideonkimaiyo254@gmail.com>
2025-05-29 20:40:47 +0300

    passes calculator challenge

---
+++ index.js
@@ -0,0 +1,36 @@
+function add() {
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function increment(a) {
+    a += 1;
+    return a;
+}
+
+function decrement(a) {
+    a -= 1;
+    return a;
+}
+
+function makeInt(a) {
+    return parseInt(a, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
